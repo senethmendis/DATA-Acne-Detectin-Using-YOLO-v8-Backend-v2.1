a1d8cc9e08ccb820d8fc5967d164bb40c38646c0
Seneth Mendis <mendis.seneth@gmail.com>
2025-05-15 03:07:00 +0530

    COMMENTED

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = FastAPI()
 #  CORS fix
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # origin
+    allow_origins=["http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- tune.py
+++ tune.py
@@ -4,9 +4,9 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load the YOLOv8 model (pre-trained)
-model = YOLO("yolov8n.pt")  # You can replace with a fine-tuned model later
+# Load the YOLOv8 model 
+model = YOLO("yolov8n.pt") 
 
-# Check if CUDA is available (use GPU if possible)
+# Check if CUDA is available and set the device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
